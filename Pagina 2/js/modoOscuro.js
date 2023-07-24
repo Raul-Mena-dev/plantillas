b32b3ec922bb7fb9d86aee7bf26979fa332d6405
Raul-Mena-dev <raulmenadev@gmail.com>
2023-07-23 20:30:31 -0600

    Navbar y carrousel

---
+++ Pagina 2/js/modoOscuro.js
@@ -0,0 +1,15 @@
+
+function cambiarTema() {
+    var htmlTag = document.getElementsByTagName('html')[0];
+    var themeAttribute = htmlTag.getAttribute('data-bs-theme');
+    var tema = document.getElementById('tema');
+    if (themeAttribute === 'dark') {
+      htmlTag.setAttribute('data-bs-theme', 'light');
+      tema.className = 'fa-solid fa-moon me-2';
+    } else {
+      htmlTag.setAttribute('data-bs-theme', 'dark');
+      tema.className = 'fa-solid fa-sun me-2';
+    }
+  }
+ 
+  
